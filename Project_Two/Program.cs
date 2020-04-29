424797a2aaf646ab5d873d888b621677609fc108
Che <checheb@dunwoody.edu>
2020-04-29 01:19:32 -0500

    made some corrections to the code, spelling errors and correction of object.

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -19,7 +19,7 @@ namespace Project_Two
             //StreamReader read;
             string line;
             string[] data;
-            List<Super_Bowl_Winners> prospectList = null; //generating a list of superbowl winners
+            List<Prospective_Player> prospectList = null; //generating a list of superbowl winners
 
             //Implementation of try catch error method 
             try
@@ -27,7 +27,7 @@ namespace Project_Two
                 FileStream input = new FileStream(PATH, FileMode.OpenOrCreate, FileAccess.ReadWrite);
                 StreamReader read = new StreamReader(input);
                 line = read.ReadLine(); // Priming input
-                prospectList = new List<Super_Bowl_Winners>();
+                prospectList = new List<Prospective_Player>();
 
                 //Implementation of looping structure to read through the records
                 while (!read.EndOfStream)
@@ -35,7 +35,7 @@ namespace Project_Two
 
                     //Reading records and creating object instances at the same time.
                     data = read.ReadLine().Split(',');
-                    prospectList.Add(new Super_Bowl_Winners(data[0], data[1], data[2], data[3], data[4], Convert.ToDouble(data[5])));//Adding objects to the end of the list, using ToDouble to convert number to a double floating point.
+                    prospectList.Add(new Prospective_Player(data[0], Convert.ToInt32(data[1], data[2], data[3], data[4], Convert.ToDouble(data[5]))));//Adding objects to the end of the list, using ToDouble to convert number to a double floating point.
                     Console.WriteLine(prospectList[prospectList.Count - 1]);// Writing and getting the number of elements contained in the list
 
                 }
@@ -79,16 +79,16 @@ namespace Project_Two
 
         public string TeamName { get; set; }
         public int YearTeamWon { get; set; }
-        public string WinningQuaterBack{ get; set; }
+        public string WinningQuarterBack{ get; set; }
         public string WinningCoach { get; set; }
         public string TheMVP { get; set; }
         public double PtDifferenceBetweenWinningAndLosingTeam { get; set; }
 
-        public  Prospective_Player(string teamName, int yearTeamWon, string winningQuaterBack, string winningCoach, string theMVP, double ptDifferenceBetweenWinningAndLosingTeam)
+        public  Prospective_Player(string teamName, int yearTeamWon, string winningQuarterBack, string winningCoach, string theMVP, double ptDifferenceBetweenWinningAndLosingTeam)
         {
             TeamName = teamName;
             YearTeamWon = yearTeamWon;
-            WinningQuaterBack = winningQuaterBack;
+            WinningQuarterBack = winningQuarterBack;
             WinningCoach = winningCoach;
             TheMVP = theMVP;
             PtDifferenceBetweenWinningAndLosingTeam = ptDifferenceBetweenWinningAndLosingTeam;
